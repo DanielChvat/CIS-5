93cf73ddc901a45b64a4086bd06482931db2faeb
DanielChvat <109172703+DanielChvat@users.noreply.github.com>
2022-07-28 17:34:34 -0700

    Add files via upload

---
+++ Project/Project1/Blackjack_GameV1/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=blackjack_gamev1
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/blackjack_gamev1
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=blackjackgamev1.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/blackjackgamev1.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=blackjack_gamev1
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/blackjack_gamev1
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=blackjackgamev1.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/blackjackgamev1.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
